b786bc515a95a7a52e91ca405115a1faf27ad76e
ALV404 <alvarold37@educastur.es>
2026-05-15 02:06:39 +0200

    Finiquitar y prefabear
    Menu principal, ajustes, creditos, cinematica

--- Assets/Scripts/UI/CerrarJuego.cs
+++ Assets/Scripts/UI/CerrarJuego.cs
@@ -6,6 +6,6 @@ public class CerrarJuego : MonoBehaviour
 {
     public void QuitGame()
     {
-        Application.Quit();
+        Application.Quit(); //Queda bastante claro creo yo, pero bueno lo explico de forma muy fácil. Tu pulsar boton. Juego ya no más.
     }
 }
